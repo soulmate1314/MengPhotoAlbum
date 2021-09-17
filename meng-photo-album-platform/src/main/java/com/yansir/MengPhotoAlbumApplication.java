d47cb9794fb2d72f36e0e47313f7517d322cfa52
yanzhen <yanzhen@e-future.com.cn>
2021-09-17 19:14:05 +0800

    能够正常打包和运行

---
+++ meng-photo-album-platform/src/main/java/com/yansir/MengPhotoAlbumApplication.java
@@ -0,0 +1,16 @@
+package com.yansir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+public class MengPhotoAlbumApplication {
+    public static void main(String[] args) {
+        System.out.println("The service to start.");
+        ConfigurableApplicationContext run = SpringApplication.run(MengPhotoAlbumApplication.class, args);
+        System.out.println(run);
+        System.out.println("The service has started.");
+    }
+}
