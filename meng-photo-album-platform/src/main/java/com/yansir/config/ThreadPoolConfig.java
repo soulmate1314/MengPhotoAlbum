87fe2a96c54b86c51206faac8e5272311063cbc9
yanzhen <yanzhen@e-future.com.cn>
2021-10-14 12:10:45 +0800

    新增多线程配置

---
+++ meng-photo-album-platform/src/main/java/com/yansir/config/ThreadPoolConfig.java
@@ -0,0 +1,46 @@
+package com.yansir.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+/**
+* @author YANSIR
+* @Description:线程池配置
+* @date 2020/12/21 15:50
+*/
+@Configuration
+public class ThreadPoolConfig
+{
+    // 核心线程池大小
+    private int corePoolSize = 5;
+
+    // 最大可创建的线程数
+    private int maxPoolSize = 10;
+
+    // 队列最大长度
+    private int queueCapacity = 1000;
+
+    // 线程池维护线程所允许的空闲时间
+    private int keepAliveSeconds = 300;
+
+    private String threadNamePrefix="yansir-pool-%d";
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor()
+    {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        executor.setThreadNamePrefix(threadNamePrefix);
+        // 线程池对拒绝任务(无线程可用)的处理策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+
+}
